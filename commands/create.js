85ef8f8e989797773208fe9242a8d54c191c6a3d
Itayra <likeosik@gmail.com>
2025-12-03 23:47:41 +0200

    quick fix

--- commands/create.js
+++ commands/create.js
@@ -2,18 +2,12 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const User = require("../models/User");
 const { logToStaff } = require("../utils/log");
 const { checkRank } = require("../utils/rankCheck");
-
-dbUser.vouches += amount;
-await dbUser.save();
-
-// Rank update
-checkRank(interaction.client, interaction.guild.id, user.id, dbUser.vouches);
-
+const Transaction = require("../models/Transaction");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("create")
-        .setDescription("Create vouches and give to a user (Admin only)")
+        .setDescription("Create vouches and give them to a user (Admin only)")
         .addUserOption(opt =>
             opt.setName("user")
                 .setDescription("User who receives the vouches")
@@ -31,39 +25,39 @@ module.exports = {
         const amount = interaction.options.getInteger("amount");
 
         if (amount <= 0)
-            return interaction.reply({ content: "Amount must be positive.", flags: 64 });
+            return interaction.reply({ content: "❌ Amount must be positive.", flags: 64 });
 
         let dbUser = await User.findOne({ userId: user.id });
         if (!dbUser) dbUser = await User.create({ userId: user.id });
 
         dbUser.vouches += amount;
-		
         await dbUser.save();
-		
-		const Transaction = require("../models/Transaction");
 
-		await Transaction.create({
-			type: "CREATE",
-			senderId: interaction.user.id,
-			recipientId: user.id,
-			amount: amount
-		});
+        // Log transaction
+        await Transaction.create({
+            type: "CREATE",
+            senderId: interaction.user.id,
+            recipientId: user.id,
+            amount
+        });
 
+        // Rank update
+        checkRank(interaction.client, interaction.guild.id, user.id, dbUser.vouches);
 
+        // User reply
         await interaction.reply({
-            content: `✅ Created **${amount} vouches** and gave them to **${user.tag}**.`,
+            content: `✅ Created **${amount}** vouches and gave them to **${user.tag}**.`,
             flags: 64
         });
 
-
-        // 🔹 Log to staff channel
+        // Staff log
         await logToStaff(
             interaction.client,
             `🧾 **CREATE**\n` +
             `Admin: **${interaction.user.tag}** (\`${interaction.user.id}\`)\n` +
             `Target: **${user.tag}** (\`${user.id}\`)\n` +
             `Amount: **${amount}**\n` +
-            `New balance: **${dbUser.vouches}**`
+            `New Balance: **${dbUser.vouches}**`
         );
     }
 };
